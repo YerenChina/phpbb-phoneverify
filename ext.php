6cfe15c90aafcdffb538e158b4ad2d0ba5f46f8b
YerenChina <521133238@qq.com>
2025-01-13 04:18:03 +0800

    删除ext.php中的手动数据清理操作，让迁移系统处理数据库和配置的清理，只保留缓存清理功能

--- ext.php
+++ ext.php
@@ -26,21 +26,6 @@ class ext extends \phpbb\extension\base
 
         $config = $this->container->get('config');
         $db = $phpbb_container->get('dbal.conn');
-        
-        // 1. 删除配置项
-        $config->delete('phoneverify_version');
-        $config->delete('phoneverify_aliyun_access_key_id');
-        $config->delete('phoneverify_aliyun_access_key_secret');
-        $config->delete('phoneverify_aliyun_sign_name');
-        $config->delete('phoneverify_aliyun_template_code');
-
-        // 2. 删除数据表
-        $db->sql_query('DROP TABLE IF EXISTS ' . PHONE_VERIFY_TABLE);
-
-        // 3. 删除模块
-        $sql = 'DELETE FROM ' . MODULES_TABLE . "
-                WHERE module_langname = 'ACP_PHONE_VERIFY'";
-        $db->sql_query($sql);
 
         // 4. 清理缓存
         $cache = $phpbb_container->get('cache');
